038446780ff15338bb20658319c79c09dfb5f236
Peter Schmucki <pebs@gmx.ch>
2020-11-16 12:18:04 +0100

    Added ShowModal as of AB17

---
+++ src/questionnaire/QuestionnaireShowDialog.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react'
+import { Button, Modal, Form, ModalHeader, FormGroup, Col, Label, Input, ModalBody } from 'reactstrap'
+
+export default class QuestionnaireShowDialog extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      showModal: false
+    }
+  }
+
+  close = () => this.setState({ showModal: false });
+
+  open = () => this.setState({ showModal: true });
+
+  render () {
+    return (
+          <div>
+            <Button color="secondary" onClick={this.open}
+                    className="float-right">Show</Button>
+            <Modal isOpen={ this.state.showModal } toggle={ this.close } size="lg"
+                        autoFocus={ false }>
+              <ModalHeader toggle={ this.close } >
+                Show Questionnaire
+              </ModalHeader>
+              <ModalBody>
+                 <Form>
+                   <FormGroup row>
+                     <Label md={ 2 } for="formTitle">
+                       Title
+                     </Label>
+                     <Col md={ 10 }>
+                       <Input type="text" id="formTitle"
+                         defaultValue={ this.props.questionnaire.title }
+                         plaintext />
+                     </Col>
+                   </FormGroup>
+
+                   <FormGroup row>
+                     <Label md={ 2 } for="formDescription">
+                       Description
+                     </Label>
+                     <Col md={ 10 }>
+                       <Input type="text" id="formDescription"
+                         defaultValue={ this.props.questionnaire.description }
+                         plaintext />
+                     </Col>
+                   </FormGroup>
+
+                   <FormGroup>
+                     <Col className="clearfix" style={{ padding: '.2rem' }}>
+                       <Button className="float-right" color="secondary"
+                        onClick={ this.close }>Close</Button>
+                     </Col>
+                   </FormGroup>
+                 </Form>
+              </ModalBody>
+            </Modal>
+          </div>
+    )
+  }
+}
