0a6cd9d3b4a01b67df52bb2b183e7e15149f4ab1
Peter Schmucki <peter.schmucki@valion.ch>
2020-11-23 13:00:32 +0100

    Changed ShowDialog to functional Component

--- src/questionnaire/QuestionnaireShowDialog.js
+++ src/questionnaire/QuestionnaireShowDialog.js
@@ -1,62 +1,57 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Button, Modal, Form, ModalHeader, FormGroup, Col, Label, Input, ModalBody } from 'reactstrap'
 
-export default class QuestionnaireShowDialog extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      showModal: false
-    }
-  }
+const QuestionnaireShowDialog = (props) => {
+  const [showModal, setShowModal] = useState(false)
 
-  close = () => this.setState({ showModal: false })
+  const close = () => setShowModal(false)
 
-  open = () => this.setState({ showModal: true })
+  const open = () => setShowModal(true)
 
-  render () {
-    return (
-      <div>
-        <Button color="secondary" onClick={this.open}
-          className="float-right">Show</Button>
-        <Modal isOpen={this.state.showModal} toggle={this.close} size="lg"
-          autoFocus={false}>
-          <ModalHeader toggle={this.close} >
-            Show Questionnaire
-              </ModalHeader>
-          <ModalBody>
-            <Form>
-              <FormGroup row>
-                <Label md={2} for="formTitle">
-                  Title
-                     </Label>
-                <Col md={10}>
-                  <Input type="text" id="formTitle"
-                    defaultValue={this.props.questionnaire.title}
-                    plaintext />
-                </Col>
-              </FormGroup>
+  return (
+    <div>
+      <Button color="secondary" onClick={open}
+        className="float-right">Show</Button>
+      <Modal isOpen={showModal} toggle={close} size="lg"
+        autoFocus={false}>
+        <ModalHeader toggle={close} >
+          Show Questionnaire
+            </ModalHeader>
+        <ModalBody>
+          <Form>
+            <FormGroup row>
+              <Label md={2} for="formTitle">
+                Title
+                    </Label>
+              <Col md={10}>
+                <Input type="text" id="formTitle"
+                  defaultValue={this.props.questionnaire.title}
+                  plaintext />
+              </Col>
+            </FormGroup>
 
-              <FormGroup row>
-                <Label md={2} for="formDescription">
-                  Description
-                     </Label>
-                <Col md={10}>
-                  <Input type="text" id="formDescription"
-                    defaultValue={this.props.questionnaire.description}
-                    plaintext />
-                </Col>
-              </FormGroup>
+            <FormGroup row>
+              <Label md={2} for="formDescription">
+                Description
+                    </Label>
+              <Col md={10}>
+                <Input type="text" id="formDescription"
+                  defaultValue={this.props.questionnaire.description}
+                  plaintext />
+              </Col>
+            </FormGroup>
 
-              <FormGroup>
-                <Col className="clearfix" style={{ padding: '.2rem' }}>
-                  <Button className="float-right" color="secondary"
-                    onClick={this.close}>Close</Button>
-                </Col>
-              </FormGroup>
-            </Form>
-          </ModalBody>
-        </Modal>
-      </div>
-    )
-  }
+            <FormGroup>
+              <Col className="clearfix" style={{ padding: '.2rem' }}>
+                <Button className="float-right" color="secondary"
+                  onClick={close}>Close</Button>
+              </Col>
+            </FormGroup>
+          </Form>
+        </ModalBody>
+      </Modal>
+    </div>
+  )
 }
+
+export default QuestionnaireShowDialog
